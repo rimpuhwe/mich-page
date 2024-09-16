b7357afff6c3eaa18b7092760a818d3b3f2c25c2
Raissa Micheline <rimpuhwemanzi@gmail.com>
2024-09-16 10:48:00 +0200

    contact me not working as wanted

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 //turning the page when clicking the arrows
 const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
 
-pageTurnBtn.forEach((el,index) =>{
-    el.onclick = () =>{
-        const pageTurnId =el.getAttribute('data-page');
+pageTurnBtn.forEach((el, index) => {
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute('data-page');
         const pageTurn = document.getElementById(pageTurnId);
-        
-        if(pageTurn.classList.contains('turn')){
+
+        if (pageTurn.classList.contains('turn')) {
             pageTurn.classList.remove('turn');
-            setTimeout(()=>{
+            setTimeout(() => {
                 pageTurn.style.zIndex = 20 - index;
-            },500)
+            }, 500)
         }
-        else{
+        else {
             pageTurn.classList.add('turn');
-            setTimeout(()=>{
+            setTimeout(() => {
                 pageTurn.style.zIndex = 20 + index;
-            },500)
-        }
-    }
-})
+            }, 500);
+        };
+    };
+});
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me');
+
+contactMeBtn.onclick = () => {
+    pages.forEach((page, index) => {
+        setTimeout(() => {
+            page.classList.add('turn');
+            setTimeout(() => {
+                page.style.zIndex = 20 + index;
+            }, 500)
+        }, (index + 1) * 2000 + 100)
+    })
+
+}
